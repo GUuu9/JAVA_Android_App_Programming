e8604b0a2b0ffcecdba5a5dd02b5abc9e8ff79ba
Uuu9 <87708661+GUuu9@users.noreply.github.com>
2022-11-29 23:32:01 +0900

    Add files via upload

---
+++ chap13.src/main/java/com/cookandroid/chap13/MainActivity.java
@@ -0,0 +1,46 @@
+package com.cookandroid.chap13;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button1 = (Button) findViewById(R.id.button1);
+        Button button2 = (Button) findViewById(R.id.button2);
+        Button button3 = (Button) findViewById(R.id.button3);
+
+        button1.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Self1301.class);
+
+                startActivity(intent);
+            }
+        });
+        button2.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Self1302.class);
+
+                startActivity(intent);
+            }
+        });
+        button3.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Self1303.class);
+
+                startActivity(intent);
+            }
+        });
+    }
+}
